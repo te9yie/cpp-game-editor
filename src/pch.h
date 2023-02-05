58d4fc874d046c23c329367c455cb7ee357e1bf1
te9yie <te9yie@users.noreply.github.com>
2023-02-05 16:37:23 +0900

    init

---
+++ src/pch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#if defined(_MSC_VER)
+#include <crtdbg.h>
+#endif
+
+#include <SDL.h>
+
+#include <cassert>
+#include <cstdlib>
+#include <memory>
+
+#include "imgui.h"
+#include "imgui_impl_sdl.h"
+#include "imgui_impl_sdlrenderer.h"
